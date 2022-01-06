68ab4e994de856e355a73bdfcb4ab312fd098f0c
Andrei Pereira <andrei.pereira@lnls.br>
2022-01-06 11:26:05 -0300

    Quench: Uncomment copper_properties function

--- scripts/quench/python/detection/materials.py
+++ scripts/quench/python/detection/materials.py
@@ -2,35 +2,35 @@ import sys as _sys
 import traceback as _traceback
 import numpy as _np
 
-# def copper_properties(T,RRR,B):
-#     """
-#        Refs.: 
-#        [1] M. McAshan, "MIITS Integrals for Copper and for Nb-46Ti"
-#        [2] KN5010 Nº4 (Davide)
-#        [3] https://www.copper.org/resources/properties/cryogenic/   """
-#     # Resistivity [Ohm.m]
-#     rho = _np.multiply(
-#             1e-8,
-#             _np.add(
-#                 _np.divide(1.545,RRR),
-#                 _np.divide(
-#                     1,
-#                     _np.add(
-#                         _np.add(
-#                             _np.divide(2.32547*1e9, _np.power(T,5)),
-#                             _np.divide(9.57137*1e5, _np.power(T,3))
-#                             ),
-#                         _np.divide(1.62735*1e2, T)
-#                         )
-#                     )
-#                 )
-#             )
-#     # Specific heat [J/kg.K] - polynomial fit obtained for temperatures up to 50 K
-#     P = [-0.000006646433277, 0.001186241294937, -0.003070534537926, 0.018727649162436, 0]
-#     c = _np.polyval(P,T)
-#     # Thermal conductivity [W/m.K]
-#     k = 
-#     return [rho, c, k]
+def copper_properties(T,RRR,B):
+    """
+       Refs.: 
+       [1] M. McAshan, "MIITS Integrals for Copper and for Nb-46Ti"
+       [2] KN5010 Nº4 (Davide)
+       [3] https://www.copper.org/resources/properties/cryogenic/   """
+    # Resistivity [Ohm.m]
+    rho = _np.multiply(
+            1e-8,
+            _np.add(
+                _np.divide(1.545,RRR),
+                _np.divide(
+                    1,
+                    _np.add(
+                        _np.add(
+                            _np.divide(2.32547*1e9, _np.power(T,5)),
+                            _np.divide(9.57137*1e5, _np.power(T,3))
+                            ),
+                        _np.divide(1.62735*1e2, T)
+                        )
+                    )
+                )
+            )
+    # Specific heat [J/kg.K] - polynomial fit obtained for temperatures up to 50 K
+    P = [-0.000006646433277, 0.001186241294937, -0.003070534537926, 0.018727649162436, 0]
+    c = _np.polyval(P,T)
+    # Thermal conductivity [W/m.K]
+    k = 0 # dummy value
+    return [rho, c, k]
 
 def copper_resistivity(T, RRR):
     # Ref.: M. McAshan, "MIITS Integrals for Copper and for Nb-46Ti "
